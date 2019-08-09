a1cbdb63af8913efa349dac01afaa219685090a5
DKuwalek <d.kuwalek@gmail.com>
2019-08-09 23:30:53 +0200

    Improve the comment about constants
     - make the comment more specific
     - clean up the styling

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,12 +1,9 @@
 console.log('This is constants example!');
 
 // const avengerId;
-/** This will fail because you need to initialize a constant after it is declared!
-Browser will not explicitly point out that constants have to be initialized. Instead it will say:
+/** This declaration of a constant 'avengerId' will fail
+because you need to initialize a constant when it is declared!
+Browser will not explicitly point out that constants have to be initialized when declared.
+Instead it will say:
 "Module parse failed: Unexpected token (lineNumber:positionOfTheSemicolon)
-You may need an appropriate loader to handle this file type."
-*/
-
-
-
-
+You may need an appropriate loader to handle this file type."*/
